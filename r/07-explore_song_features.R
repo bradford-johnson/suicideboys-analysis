d43815b8464d18d3873dae022e2c0cf17133e69c
bradfordjohnson <bradford.lewis.johnson@outlook.com>
2023-04-30 15:53:22 -0400

    joined data

--- r/07-explore_song_features.R
+++ r/07-explore_song_features.R
@@ -4,19 +4,33 @@
   )
   
 # load data
-  tracks <- read_csv("data/spotify_track_features.csv")
+  track_features <- read_csv("data/spotify_track_features.csv")
+  tracks <- read_csv("data/tracks_data_spotify.csv")
+  albums <- read_csv("data/album_data_spotify.csv")
+  
+# join data
+  tracks_joined <- albums |>
+    left_join(tracks, by = c("id" = "album_id")) |>
+    mutate(album_id = id, album_name = name.x, track_id = id.y,
+           track_name = name.y, artist_1 = artist_1.y, artist_2 = artist_2.y) |>
+    select(album_id, album_name, release_date, total_tracks,
+           track_id, track_name, explicit, track_number,
+           artist_1, artist_2, artist_3, artist_4)
+  
+  tracks_and_features_joined <- tracks_joined |>
+    inner_join(track_features, by = c("track_id" = "id"))
   
 # explore for insights
   
-  tracks |>
+  track_features |>
     ggplot(aes(x = valence, y = energy)) +
     geom_point(aes(size = loudness, alpha = .5))
 
-  tracks |>
+  track_features |>
     ggplot(aes(x = duration_ms, y = tempo)) +
     geom_point(aes(size = energy, alpha = .5))
   
-  tracks |>
+  track_features |>
     ggplot(aes(x = duration_ms, y = danceability)) +
     geom_point(aes(size = energy, alpha = .5))
 
